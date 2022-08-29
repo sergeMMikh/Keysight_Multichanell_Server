3d1a95977e3555e1b60383e9d0615af4fb979813
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-29 12:33:24 +0100

    fixed cls_SocketServer.py

--- cls_SocketServer.py
+++ cls_SocketServer.py
@@ -1,16 +1,12 @@
 import socket
 import select
 
-"""
-This the block of this code taken from:
-http://theembeddedlab.com/tutorials/simple-socket-server-python/
-"""
-
+#http://theembeddedlab.com/tutorials/simple-socket-server-python/
 
 class SocketServer:
     """ Simple socket server that listens to one single client. """
-
-    def __init__(self, host='127.0.0.1', port=9008):
+ 
+    def __init__(self, host = '0.0.0.0', port = 9008):
         """ Initialize the server with a host and port to listen to. """
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,102 +14,92 @@ class SocketServer:
         self.port = port
         self.sock.bind((host, port))
         self.sock.listen(1)
-        self.client_sock = str()
-        self.client_addr = str()
-
+ 
     def close(self):
         """ Close the server socket. """
         print('Closing server socket (host {}, port {})'.format(self.host, self.port))
         if self.sock:
             self.sock.close()
             self.sock = None
-
-    def run_server(self, m_data):
+ 
+    def run_server(self, M_data):
         """ Accept and handle an incoming connection. """
         print('Starting socket server (host {}, port {})'.format(self.host, self.port))
-
-        self.client_sock, self.client_addr = self.sock.accept()
-
-        print('Client {} connected'.format(self.client_addr))
-
+ 
+        SocketServer.client_sock, SocketServer.client_addr = self.sock.accept()
+ 
+        print('Client {} connected'.format(SocketServer.client_addr))
+ 
         stop = False
         while not stop:
-            if self.client_sock:
+            if SocketServer.client_sock:
                 # Check if the client is still connected and if data is available:
                 try:
-                    rdy_read, rdy_write, sock_err = select.select([self.client_sock, ], [], [])
+                    rdy_read, rdy_write, sock_err = select.select([SocketServer.client_sock,], [], [])
                 except select.error:
-                    print('Select() failed on socket with {}'.format(self.client_addr))
+                    print('Select() failed on socket with {}'.format(SocketServer.client_addr))
                     return 1
-
+ 
                 if len(rdy_read) > 0:
-                    read_data = self.client_sock.recv(255)
+                    read_data = SocketServer.client_sock.recv(255)
                     # Check if socket has been closed
                     if len(read_data) == 0:
-                        print('{} closed the socket.'.format(self.client_addr))
+                        print('{} closed the socket.'.format(SocketServer.client_addr))
                         stop = True
                     else:
                         print('>>> Received: {}'.format(read_data.rstrip()))
                         if read_data.rstrip() == 'quit':
                             stop = True
                         else:
-                            self.client_sock.send(m_data.encode())
+                            SocketServer.client_sock.send(M_data.encode())
             else:
                 print("No client is connected, SocketServer can't receive data")
                 stop = True
-
+ 
         # Close socket
-        print('Closing connection with {}'.format(self.client_addr))
-        self.client_sock.close()
+        print('Closing connection with {}'.format(SocketServer.client_addr))
+        SocketServer.client_sock.close()
         return 0
 
-    def recv_server_data(self):
+    def send_server_data(self):
         """ Accept and handle an incoming connection. """
         print('Starting socket server (host {}, port {})'.format(self.host, self.port))
-
-        self.client_sock, self.client_addr = self.sock.accept()
-
-        print('Client {} connected'.format(self.client_addr))
-
+ 
+        SocketServer.client_sock, SocketServer.client_addr = self.sock.accept()
+ 
+        print('Client {} connected'.format(SocketServer.client_addr))
+ 
         stop = False
         while not stop:
-            if self.client_sock:
+            if SocketServer.client_sock:
                 # Check if the client is still connected and if data is available:
                 try:
-                    rdy_read, rdy_write, sock_err = select.select([self.client_sock,], [], [])
+                    rdy_read, rdy_write, sock_err = select.select([SocketServer.client_sock,], [], [])
                 except select.error:
-                    print('Select() failed on socket with {}'.format(self.client_addr))
+                    print('Select() failed on socket with {}'.format(SocketServer.client_addr))
                     return 1
-
+ 
                 if len(rdy_read) > 0:
-                    read_data = self.client_sock.recv(1024)
+                    read_data = SocketServer.client_sock.recv(1024)
                     # Check if socket has been closed
                     if len(read_data) == 0:
-                        print('{} closed the socket.'.format(self.client_addr))
+                        print('{} closed the socket.'.format(SocketServer.client_addr))
                         stop = True
                     else:
                         print('>>> Received: {}'.format(read_data.rstrip()))
                         stop = True
             else:
                 print("No client is connected, SocketServer can't receive data")
-                stop = True
+                stop = True 
                 # Close socket
-                print('Closing connection with {}'.format(self.client_addr))
-                self.client_sock.close()
-
+                print('Closing connection with {}'.format(SocketServer.client_addr))
+                SocketServer.client_sock.close()
+                
         return read_data
 
-    def send_server_data(self, m_data):
-        self.client_sock.send(m_data.encode())
+    def send_str_server_data(self, M_data):
+        SocketServer.client_sock.send(M_data.encode())
         # Close socket
-        print('Closing connection with {}'.format(self.client_addr))
-        self.client_sock.close()
-        return 0
-
-    def send_str_server_data(self, m_data):
-        print('M_data', m_data)
-        self.client_sock.send(m_data.encode())
-        # Close socket
-        print('Closing connection with {}'.format(self.client_addr))
-        self.client_sock.close()
+        print('Closing connection with {}'.format(SocketServer.client_addr))
+        SocketServer.client_sock.close()
         return 0
